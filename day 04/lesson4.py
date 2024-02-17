7194b5419c80a6b90dce2268b59cf2aa56db65d6
andria780 <akobidzeandira@gmail.com>
2024-02-17 13:56:13 +0400

    second commit

---
+++ day 04/lesson4.py
@@ -0,0 +1,6 @@
+name = "andria"     # string = ""
+age = 11  # integer = yvela riccxvi 
+lastname = "akobidze"
+age10 = age + 10
+print("me mqvia " , name , " " , lastname , " da var " , str(age) , " wlis")
+print("10 wlis mere viqnebi" , age10 , " wlis")
