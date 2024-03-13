9c01172199749db2aba7a16adf1f199ac1287690
andria780 <akobidzeandira@gmail.com>
2024-03-13 11:32:25 +0400

    commit

---
+++ day 12/homework.py
@@ -0,0 +1,13 @@
+score = int(input(" what did you get at test:    "))
+
+if score == 10:
+    print("good job")
+
+elif score == 8 or score == 9:
+    print("your son needs to work")
+
+elif score == 5:
+    print("your son has really hard problems, so work on it")
+
+elif score < 5:
+    print("your son is getting kicked from the school")
